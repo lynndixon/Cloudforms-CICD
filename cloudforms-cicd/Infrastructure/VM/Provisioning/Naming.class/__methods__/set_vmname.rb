ded978d522f5f7aedf8f49954601dbd2393f06f7
Lynn Dixon <ldixon@ldixon-mbp.localdomain>
2019-05-14 17:54:41 -0400

    test 2

--- cloudforms-cicd/Infrastructure/VM/Provisioning/Naming.class/__methods__/set_vmname.rb
+++ cloudforms-cicd/Infrastructure/VM/Provisioning/Naming.class/__methods__/set_vmname.rb
@@ -37,7 +37,7 @@ begin
   log(:info, "prov object found is: #{prov.inspect} ")
   vm_name = nil
   vm_name = prov.get_option(:vm_target_name).to_s.strip
-  vm_prefix = "TEST1" + $evm.object['vm_prefix']
+  vm_prefix = "TEST2" + $evm.object['vm_prefix']
 
   # Simple handler to generate a vmname if none is supplied by a service dialog or other means
   if vm_name.blank? || vm_name == 'changeme'
